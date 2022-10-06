d347e1b9921eb5e2ae4798b238254bdb2e664f72
ttasjwi <ttasjwi920@gmail.com>
2022-10-06 20:29:01 +0900

    PostService 개발, 테스트

--- src/main/java/com/ttasjwi/jojulduBook/service/post/PostService.java
+++ src/main/java/com/ttasjwi/jojulduBook/service/post/PostService.java
@@ -2,6 +2,8 @@ package com.ttasjwi.jojulduBook.service.post;
 
 import com.ttasjwi.jojulduBook.domain.post.Post;
 import com.ttasjwi.jojulduBook.domain.post.PostRepository;
+import com.ttasjwi.jojulduBook.exception.PostNotFoundException;
+import com.ttasjwi.jojulduBook.web.dto.PostResponseDto;
 import com.ttasjwi.jojulduBook.web.dto.PostSaveRequestDto;
 import com.ttasjwi.jojulduBook.web.dto.PostSaveResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +21,12 @@ public class PostService {
         Post savePost = requestDto.toEntity();
         postRepository.save(savePost);
         return new PostSaveResponseDto(savePost.getId());
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponseDto findById(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 게시글이 없습니닷...! "+postId));
+        return new PostResponseDto(post);
     }
 }
--- src/test/java/com/ttasjwi/jojulduBook/service/post/PostServiceTest.java
+++ src/test/java/com/ttasjwi/jojulduBook/service/post/PostServiceTest.java
@@ -2,11 +2,17 @@ package com.ttasjwi.jojulduBook.service.post;
 
 import com.ttasjwi.jojulduBook.domain.post.Post;
 import com.ttasjwi.jojulduBook.domain.post.PostRepository;
+import com.ttasjwi.jojulduBook.exception.PostNotFoundException;
+import com.ttasjwi.jojulduBook.web.dto.PostResponseDto;
 import com.ttasjwi.jojulduBook.web.dto.PostSaveRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -38,5 +44,44 @@ class PostServiceTest {
 
         postService.save(requestDto);
         verify(postRepository).save(any(Post.class)); // save 메서드가 호출됐는 지 검증
+    }
+
+    @Test
+    @DisplayName("findById 메서드에서, 존재하는 id로 조회 → Dto 반환")
+    public void findByIdSuccessTest() {
+        Long postId = 1L;
+
+        String title = "title";
+        String content = "content";
+        String author = "author";
+
+        Post findPost = Post.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        when(postRepository.findById(postId))
+                .thenReturn(Optional.of(findPost));
+
+        PostResponseDto responseDto = postService.findById(postId);
+
+        assertThat(responseDto.getTitle()).isEqualTo(title);
+        assertThat(responseDto.getContent()).isEqualTo(content);
+        assertThat(responseDto.getAuthor()).isEqualTo(author);
+
+        verify(postRepository).findById(postId);
+    }
+
+    @Test
+    @DisplayName("findById 메서드에서, 존재하지 않는 id로 조회 → NoSuchPostException 발생")
+    public void findByIdFailureTest() {
+        Long postId = 1L;
+
+        when(postRepository.findById(postId))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> postService.findById(1L)).isInstanceOf(PostNotFoundException.class);
+        verify(postRepository).findById(1L);
     }
 }
