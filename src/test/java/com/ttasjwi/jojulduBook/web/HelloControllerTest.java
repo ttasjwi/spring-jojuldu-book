5279abce816bd883726f0d5141d24b68aeab2dd2
ttasjwi <ttasjwi920@gmail.com>
2022-10-06 14:09:38 +0900

    HelloController - Dto 응답 기능 추가, 테스트

--- src/test/java/com/ttasjwi/jojulduBook/web/HelloControllerTest.java
+++ src/test/java/com/ttasjwi/jojulduBook/web/HelloControllerTest.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = HelloController.class) // @WebMvcTest : web layer의 필요한 빈만 가져와서 테스트
 class HelloControllerTest {
@@ -24,5 +24,19 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("'/hello/dto'로 파라미터 전달 요청 -> dto가 응답된다.")
+    public void helloDtoTest() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                    .param("name", name) // 파라미터 지정
+                    .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk()) // status
+                .andExpect(jsonPath("$.name", is(name))) // 응답 json
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
