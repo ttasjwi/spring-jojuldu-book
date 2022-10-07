c6743999c6dc3061e0a150d714f1a228974a2735
ttasjwi <ttasjwi920@gmail.com>
2022-10-07 15:50:25 +0900

    Post 등록, 수정 시간 추적 기능 테스트

--- src/test/java/com/ttasjwi/jojulduBook/domain/post/PostRepositoryTest.java
+++ src/test/java/com/ttasjwi/jojulduBook/domain/post/PostRepositoryTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -16,6 +19,9 @@ class PostRepositoryTest {
 
     @Autowired
     PostRepository postRepository;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     @DisplayName("게시글 저장 및 불러오기")
@@ -42,5 +48,38 @@ class PostRepositoryTest {
         assertThat(findPost.getTitle()).isEqualTo(title);
         assertThat(findPost.getContent()).isEqualTo(content);
         assertThat(findPost.getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void save_BaseTime_Entity() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 01, 01, 0, 0, 0);
+
+        Post savePost = Post.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build();
+
+        postRepository.save(savePost);
+        em.flush();
+
+        Post findPost = postRepository.findById(savePost.getId()).get();
+        LocalDateTime createdDateTime1 = findPost.getCreatedDateTime();
+        LocalDateTime modifiedDateTime1 = findPost.getModifiedDateTime();
+        log.info("[First] {}", findPost);
+
+        findPost.update("updateTitle", "updateContent");
+        em.flush();
+
+        Post updatePost = postRepository.findById(savePost.getId()).get();
+        LocalDateTime createdDateTime2 = updatePost.getCreatedDateTime();
+        LocalDateTime modifiedDateTime2 = updatePost.getModifiedDateTime();
+        log.info("[After Update] {}", updatePost);
+
+        assertThat(createdDateTime1).isAfter(now);
+        assertThat(modifiedDateTime1).isAfter(now);
+        assertThat(createdDateTime2).isEqualTo(createdDateTime1);
+        assertThat(modifiedDateTime2).isAfter(modifiedDateTime1);
     }
 }
